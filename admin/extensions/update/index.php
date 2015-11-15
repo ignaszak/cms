fe5daeac4f8fefb96fc12b2117c9024a56b7432a
ignaszak <tomek.ignaszak@gmail.com>
2015-11-15 13:34:27 +0100

    Update

--- admin/extensions/update/index.php
+++ admin/extensions/update/index.php
@@ -1,7 +1,2 @@
 <?php
 
-use VIPSoft\Unzip\Unzip;
-
-$unzipper  = new Unzip();
-$zipFilePath = 'https://github.com/ignaszak/cms/archive/master.zip';
-$filenames = $unzipper->extract($zipFilePath, __DIR__ . '/tmp/');
