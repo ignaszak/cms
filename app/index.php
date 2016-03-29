f09a6e8722f10277988526491a7de443687d0395
ignaszak <tomek.ignaszak@gmail.com>
2016-03-29 21:44:41 +0200

    Add router info display

--- app/index.php
+++ app/index.php
@@ -1,4 +1,6 @@
 <?php
+
+use App\Resource\RouterStatic;
 try {
 
     require __DIR__ . '/conf/constants.php';
@@ -6,6 +8,24 @@ try {
     $app = new App\App;
     $app->validConf();
     $app->run();
+
+
+    ?>
+
+    <div style="position: fixed; top: 50px;">
+    <pre>
+<b>Name:</b> <?php echo RouterStatic::getName(); ?>
+
+<b>Group:</b> <?php echo RouterStatic::getGroup(); ?>
+
+<b>Controller:</b> <?php echo RouterStatic::getController(); ?>
+
+<b>Params:</b> <?php print_r(RouterStatic::getParams()); ?>
+    </pre>
+    </div>
+
+    <?php
+
 } catch (Exception $e) {
     $app->catchException($e);
 }
