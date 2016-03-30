54600be42b65e06371ba08342877eb5a4fdaf7d8
ignaszak <tomek.ignaszak@gmail.com>
2016-03-31 00:04:18 +0200

    Add property validation

--- app/model/App/Conf/Configuration.php
+++ app/model/App/Conf/Configuration.php
@@ -1,12 +1,6 @@
 <?php
 namespace App\Conf;
 
-use Conf\DB\DBSettings;
-
-$baseDIR = dirname(dirname(dirname(dirname(__DIR__))));
-
-include "{$baseDIR}/vendor/davcs86/php-mysqlimporter/php-mysqlimporter.php";
-
 class Configuration
 {
 
@@ -28,7 +22,8 @@ class Configuration
      */
     public static function setAdress(string $siteAdress)
     {
-        $siteAdress = substr($siteAdress, - 1) == "/" ? substr($siteAdress, 0, - 1) : $siteAdress;
+        $siteAdress = substr($siteAdress, - 1) == "/" ?
+            substr($siteAdress, 0, - 1) : $siteAdress;
         $array = explode('/', $siteAdress);
         self::$baseUrl = $array[0] . '//' . $array[2] . '/';
         unset($array[0]);
@@ -55,16 +50,73 @@ class Configuration
         return self::$requestUrl;
     }
 
+    /**
+     *
+     * @param string $host
+     * @param string $dbName
+     * @param string $user
+     * @param string $password
+     * @return boolean
+     */
+    public static function checkDatabase(
+        string $host,
+        string $dbName,
+        string $user,
+        string $password
+    ): string {
+        try {
+            new \PDO(
+                "mysql:host={$host};dbname={$dbName}",
+                $user,
+                $password
+            );
+            return 'true';
+        } catch (\PDOException $e) {
+            return 'false';
+        }
+    }
+
+    /**
+     *
+     * @param string $login
+     * @return boolean
+     */
+    public static function checkLogin(string $login): string
+    {
+        return (ctype_alnum($login) && strlen($login) >= 2) ? '' : 'login';
+    }
+
+    /**
+     *
+     * @param string $password
+     * @return boolean
+     */
+    public static function checkPassword(string $password): string
+    {
+        return (ctype_alnum($password) && strlen($password) >= 8)
+            ? '' : 'password';
+    }
+
+    /**
+     *
+     * @param string $email
+     * @return boolean
+     */
+    public static function checkEmail(string $email): string
+    {
+        return filter_var($email, FILTER_VALIDATE_EMAIL) ? '' : 'email';
+    }
+
     public static function createDataBase()
     {
         $mysqlImport = new \MySQLImporter(
-            DBSettings::DB_HOST,
-            DBSettings::DB_USER,
-            DBSettings::DB_PASSWORD
+            \Conf\DB\DBSettings::DB_HOST,
+            \Conf\DB\DBSettings::DB_USER,
+            \Conf\DB\DBSettings::DB_PASSWORD
         );
         $mysqlImport->doImport(
             __BASEDIR__ . "/data/cache/tmp/tmp_db.sql",
-            DBSettings::DB_NAME
+            \Conf\DB\DBSettings::DB_NAME
         );
     }
 }
