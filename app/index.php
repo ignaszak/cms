3a9a541eefbe4a9df9b7a81aa350e4b9bfee8a44
ignaszak <tomek.ignaszak@gmail.com>
2016-05-02 00:49:47 +0200

    Update

--- app/index.php
+++ app/index.php
@@ -1,21 +1,11 @@
 <?php
 
 try {
-
     require __DIR__ . '/conf/constants.php';
     require __BASEDIR__ . '/vendor/autoload.php';
     $app = new App\App;
     $app->validConf();
     $app->run();
-
-    $router = Ignaszak\Registry\RegistryFactory::start()->get('http')->router;
-    echo "<div style=\"position:fixed; top:50px; right:0px;\"><pre>";
-    echo $router->group() . '<br>';
-    echo $router->name() . '<br>';
-    echo $router->controller();
-    print_r($router->all());
-    echo "</pre></div>";
-
 } catch (Exception $e) {
     $app->catchException($e);
 }
